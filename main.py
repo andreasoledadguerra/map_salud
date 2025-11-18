8fe1c2056b0d0b3b0e3b6b6df9612d759e0c937b
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-17 21:32:25 -0300

    feat: add function to calculate distance between two points using Haversine formula

--- main.py
+++ main.py
@@ -23,6 +23,20 @@ def load_data(path="establecimientos-salud-publicos.csv"):
 
 df = load_data()
 
+# Haversine distance function
+def haversine_vectorized(lat1, lon1, lat2_arr, lon2_arr):
+    
+    R = 6371  # Radius of the Earth in kilometers
+    lat1_r = np.radians(lat1)
+    lon1_r = np.radians(lon1)
+    lat2_r = np.radians(lat2_arr)
+    lon2_r = np.radians(lon2_arr)
 
+    dlat = lat2_r - lat1_r
+    dlon = lon2_r - lon1_r
+
+    a = np.sin(dlat / 2)**2 + np.cos(lat1_r) * np.cos(lat2_r) * np.sin(dlon / 2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    return R * c
 
 
