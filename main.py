b9765100ed2e813fd9eb4fa8f136d9574e8d0537
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-18 13:16:36 -0300

    chore: import pydantic and typing

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ import uvicorn
 import streamlit as st
 
 from fastapi import FastAPI
-from streamlit_folium import st_folium
 from folium.plugins import MarkerCluster
+from pydantic import BaseModel
+from typing import Any, Dict, List
+from streamlit_folium import st_folium
 
 
 #Create FastAPI app
@@ -21,6 +23,14 @@ BASE_URL_MAP_SALUD = "http://localhost:8501"
 #Streamlit API endpoint to get data
 url_map_salud = f"{BASE_URL_MAP_SALUD}/salud"
 
+# Pydantic response model for FastAPI endpoint
+#class SaludResponseModel(BaseModel):
+#    lat: float
+#    lon: float
+#    radius_km: float
+#    top_n: int
+#    results: List[Dict[str, Any]]
+#
 # Configure page
 st.set_page_config(page_title="Map Salud", layout="wide", page_icon="🏥")
 
