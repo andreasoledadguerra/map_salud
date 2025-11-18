bfa3fb23e228369595d96119c50835ad891c22e8
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-18 19:48:22 -0300

    feat: add function haversine_vectorized()

--- api.py
+++ api.py
@@ -34,7 +34,20 @@ class SaludResponseModel(BaseModel):
     request: SaludRequestModel
     results: List[SaludResultModel]
     
+# Haversine distance function
+def haversine_vectorized(lat1, lon1, lat2_arr, lon2_arr):
+    R = 6371
+    lat1_r = np.radians(lat1)
+    lon1_r = np.radians(lon1)
+    lat2_r = np.radians(lat2_arr)
+    lon2_r = np.radians(lon2_arr)
 
+    dlat = lat2_r - lat1_r
+    dlon = lon2_r - lon1_r
+
+    a = np.sin(dlat / 2)**2 + np.cos(lat1_r) * np.cos(lat2_r) * np.sin(dlon / 2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    return R * c
     
 # Decorator to define FastAPI endpoint
 #@app.get("/establecimientos", response_model=SaludResponseModel)
