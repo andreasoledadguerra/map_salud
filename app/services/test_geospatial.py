16c59744aaa47bb37af9ecb8b1c155176c01fc3a
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-27 10:12:03 -0300

    refactor: change expected values for accuracy

--- app/services/test_geospatial.py
+++ app/services/test_geospatial.py
@@ -14,25 +14,8 @@ def test_haversine_vectorized():
     distances = haversine_vectorized(lat1, lon1, lat2_arr, lon2_arr)
     
     # Expected distances calculated manually or from a reliable source 
-    expected_distances = np.array([0.0, 157.24938127194397, 314.4748056038168])
+    expected_distances = np.array([0.0, 155.941215, 311.622025])
 
     # Assert that the calculated distances are close to the expected distances
     np.testing.assert_allclose(distances, expected_distances, rtol=1e-3)
     
-
-
-
-# Haversine function
-#def haversine_vectorized(lat1:float, lon1:float, lat2_arr:float, lon2_arr:float)->float:
-#    R = 6371
-#    lat1_r = np.radians(lat1)
-#    lon1_r = np.radians(lon1)
-#    lat2_r = np.radians(lat2_arr)
-#    lon2_r = np.radians(lon2_arr)
-#
-#    dlat = lat2_r - lat1_r
-#    dlon = lon2_r - lon1_r
-#
-#    a = np.sin(dlat / 2)**2 + np.cos(lat1_r) * np.cos(lat2_r) * np.sin(dlon / 2)**2
-#    c = 2 * np.arcsin(np.sqrt(a))
-#    return R * c
