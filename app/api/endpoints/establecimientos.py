b7161ea357ecd019e1e742ac35002ef11f3a52c8
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-27 13:14:55 -0300

    feat:  set endpoint to get  public health facilities

--- app/api/endpoints/establecimientos.py
+++ app/api/endpoints/establecimientos.py
@@ -14,36 +14,37 @@ app = FastAPI()
 df = load_data()
 coords_rad = np.vstack([np.radians(df["lat"].values), np.radians(df["long"].values)]).T
 tree = BallTree(coords_rad, metric="haversine")
-#
-#@app.post("/api/salud", response_model=SaludResponseModel)
-#def post_establecimientos(req: SaludRequestModel) -> SaludResponseModel:
-#    lat = req.lat
-#    long = req.long 
-#    radius_km = req.radius_km
-#    top_n = req.top_n
-#
-#    point_rad = np.radians([[lat, long]])
-#    radius_rad = radius_km / 6371.0
-#    indices = tree.query_radius(point_rad, r=radius_rad)[0]
-#
-#    distances_km = haversine_vectorized(
-#        lat, long, 
-#        df.iloc[indices]["lat"].values, 
-#        df.iloc[indices]["long"].values
-#    )
-#
-#    df_results = df.iloc[indices].copy()
-#    df_results["distance_km"] = distances_km
-#    df_results = df_results.sort_values(by="distance_km").head(top_n)
-#
-#    results = [
-#        SaludResultModel(
-#            lat=row["lat"],
-#            long=row["long"], 
-#            fna=row["fna"],
-#            distance_km=row["distance_km"]
-#        )
-#        for _, row in df_results.iterrows()
-#    ]
-#
-#    return SaludResponseModel(request=req, results=results)
+
+# Endpoint para establecimientos de salud
+@app.post("/api/salud", response_model=SaludResponseModel)
+def post_establecimientos(req: SaludRequestModel) -> SaludResponseModel:
+    lat = req.lat
+    long = req.long 
+    radius_km = req.radius_km
+    top_n = req.top_n
+
+    point_rad = np.radians([[lat, long]])
+    radius_rad = radius_km / 6371.0
+    indices = tree.query_radius(point_rad, r=radius_rad)[0]
+
+    distances_km = haversine_vectorized(
+        lat, long, 
+        df.iloc[indices]["lat"].values, 
+        df.iloc[indices]["long"].values
+    )
+
+    df_results = df.iloc[indices].copy()
+    df_results["distance_km"] = distances_km
+    df_results = df_results.sort_values(by="distance_km").head(top_n)
+
+    results = [
+        SaludResultModel(
+            lat=row["lat"],
+            long=row["long"], 
+            fna=row["fna"],
+            distance_km=row["distance_km"]
+        )
+        for _, row in df_results.iterrows()
+    ]
+
+    return SaludResponseModel(request=req, results=results)
