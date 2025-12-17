c8ef7723e7a0533240101e047cf6ae73f06b58f9
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-12-17 11:34:47 -0300

    chore: delete function_data()

--- process_stream.py
+++ process_stream.py
@@ -2,13 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-
-def load_data(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path, delimiter=";")
-    df = df[["lat", "long", "fna"]].copy()
-    df["lat"] = df["lat"].astype(float)
-    df["long"] = df["long"].astype(float)
-    df["fna"] = df["fna"].astype(str)
-    
-    return df
-
+## Load data 
+#def load_data(path=PATH) -> pd.DataFrame:
+#    df = pd.read_csv(PATH, delimiter=";")
+#    df = df[[COL_LAT , COL_LONG, EST_SALUD]].copy()
+#    df[COL_LAT] = df[COL_LAT].astype(float)
+#    df[COL_LONG] = df[COL_LONG].astype(float)
+#    df[EST_SALUD] = df[EST_SALUD].astype(str)
+#    
+#    return df
